a746fd1eff964623794423f70e976f68f73b44ef
alekseichistiakov <i@alexeychistyakov.ru>
2025-10-24 11:23:24 +0200

    Создал DTO для ответов с сервера

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/responses/AddressDto.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.data.dto.responses
+
+import com.google.gson.annotations.SerializedName
+
+data class AddressDto(
+    @SerializedName("city")
+    val city: String?,
+
+    @SerializedName("street")
+    val street: String?,
+
+    @SerializedName("building")
+    val building: String?,
+
+    @SerializedName("fullAddress")
+    val fullAddress: String?
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/responses/EmployerDto.kt
@@ -0,0 +1,14 @@
+package ru.practicum.android.diploma.data.dto.responses
+
+import com.google.gson.annotations.SerializedName
+
+data class EmployerDto(
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("name")
+    val name: String,
+
+    @SerializedName("logo")
+    val logo: String?
+)
