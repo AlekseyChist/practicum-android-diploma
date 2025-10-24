ac06e3956550d3f3d05836d4752b00ab6f851016
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-25 00:46:19 +0300

    3.Bot Git rygaetsa na strocki, probuem takoi variant s classom. ctrl+alt+L

--- app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.util.connectivity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.util.Log
 
 class ConnectivityChecker(private val context: Context) {
 
