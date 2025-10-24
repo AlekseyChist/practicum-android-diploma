2c8a1cd19f75d13a6c61de73fe4c0b767cbb7f8d
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-25 00:37:07 +0300

    3.Bot Git rygaetsa na strocki, probuem takoi variant s classom

--- app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
@@ -7,8 +7,10 @@ import android.net.NetworkCapabilities
 class ConnectivityChecker(private val context: Context) {
 
     fun isConnected(): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
                 ?: return false
 
         return when {
