fd5b61b19ee592f331d90db7d311b74c0c0b28df
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-25 00:30:54 +0300

    2.add fun isConnected()

--- app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
@@ -8,7 +8,6 @@ fun isConnected(context: Context): Boolean {
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
             ?: return false
-
     return when {
         capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
         capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
