6aaf9631c5f9924f527bb33ea96add7d708af89b
Evgeniy <tokareev.evgeniy@gmail.com>
2025-10-25 20:07:36 +0300

    Theme добавил цвета и скелет темы

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlackUniversal = Color(0xFF1A1B22)
+val WhiteUniversal = Color(0xFFFDFDFD)
+val Blue = Color(0xFF3772E7)
+val Red = Color(0xFFF56B6C)
+val Gray = Color(0xFFAEAFB4)
+val LightGray = Color(0xFFE6E8EB)
+val BackgroundOverlay = Color(0x801A1B22)
--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
@@ -1,2 +1,46 @@
 package ru.practicum.android.diploma.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorPalette = lightColorScheme(
+    primary = Blue,
+    onPrimary = WhiteUniversal,
+    secondary = Gray,
+    onSecondary = BlackUniversal,
+    background = WhiteUniversal,
+    onBackground = BlackUniversal,
+    surface = LightGray,
+    onSurface = Gray,
+    error = Red,
+    onError = WhiteUniversal
+)
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Blue,
+    onPrimary = BlackUniversal,
+    secondary = LightGray,
+    onSecondary = BlackUniversal,
+    background = BlackUniversal,
+    onBackground = WhiteUniversal,
+    surface = Gray,
+    onSurface = WhiteUniversal,
+    error = Red,
+    onError = BlackUniversal
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
