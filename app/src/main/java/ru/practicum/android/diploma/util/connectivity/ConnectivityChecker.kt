bfeddc78c8cfa3096e5f82a3c2176efd34c4ec98
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-26 13:55:26 +0300

    4. Ispravil BAG, gde polychali true pri lybom podklychenii WiFi. Spasibo GPT za pomozh s Detekt

--- app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/connectivity/ConnectivityChecker.kt
@@ -10,10 +10,8 @@ class ConnectivityChecker(private val context: Context) {
         val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = manager.activeNetwork
         val caps = network?.let { manager.getNetworkCapabilities(it) }
-
         val hasInternet = caps?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
         val isValidated = caps?.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) == true
-
         return hasInternet && isValidated
     }
 }
