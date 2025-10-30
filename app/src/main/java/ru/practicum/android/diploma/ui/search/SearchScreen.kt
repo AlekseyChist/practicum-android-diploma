bbd7bcf64aaf05259bd0cad550ed241d57e51517
VankaGam <122173219+VankaGam@users.noreply.github.com>
2025-10-30 23:44:47 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchScreen.kt
@@ -16,11 +16,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.material.icons.outlined.FilterList
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
