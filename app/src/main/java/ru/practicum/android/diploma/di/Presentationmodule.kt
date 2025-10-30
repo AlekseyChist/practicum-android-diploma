107c195f9966030856a4d226ea095f721079f9ce
alekseichistiakov <i@alexeychistyakov.ru>
2025-10-30 11:18:17 +0100

    Создал модуль для ViewModels

---
+++ app/src/main/java/ru/practicum/android/diploma/di/Presentationmodule.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.practicum.android.diploma.presentation.vacancy.VacancyDetailViewModel
+
+/**
+ * Koin модуль для Presentation слоя (ViewModels)
+ */
+val presentationModule = module {
+
+    // ViewModel для экрана деталей вакансии
+    viewModel {
+        VacancyDetailViewModel(
+            getVacancyDetailsUseCase = get()
+        )
+    }
+}
