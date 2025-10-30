afde2859892da7c87aa4911a393859d3fc8a24c9
Evgeniy <tokareev.evgeniy@gmail.com>
2025-10-30 18:37:10 +0300

    Vacancy detail

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyScreen.kt
@@ -2,35 +2,69 @@ package ru.practicum.android.diploma.ui.vacancy
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VacancyScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground
+                ),
+                title = {
+                    Text(
+                        modifier = Modifier
+                            ,
+                        text = stringResource(R.string.vacancy),
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                },
+                modifier = Modifier.sizeIn(minHeight = 56.dp),
+                windowInsets = WindowInsets.statusBars
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.background,
+        contentWindowInsets = remember { WindowInsets(0, 0, 0, 0) }
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .padding(top = 40.dp),
-            text = stringResource(R.string.vacancy),
-            style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.onBackground
-        )
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                    end = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
+                )
+        ) {
+
+        }
     }
+
 }
 
 @Preview(showBackground = true)
