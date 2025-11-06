e629ed8493b4d8205bc2d942bf85091b940d7f2a
alekseichistiakov <i@alexeychistyakov.ru>
2025-11-06 10:09:00 +0100

    Обновил перезентейшн

--- app/src/main/java/ru/practicum/android/diploma/di/Presentationmodule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/Presentationmodule.kt
@@ -3,6 +3,8 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
+import ru.practicum.android.diploma.presentation.filters.FiltersSettingsViewModel
+import ru.practicum.android.diploma.presentation.filters.IndustryViewModel
 import ru.practicum.android.diploma.presentation.search.SearchViewModel
 import ru.practicum.android.diploma.presentation.vacancy.VacancyDetailViewModel
 
@@ -35,6 +37,22 @@ val presentationModule = module {
     viewModel {
         SearchViewModel(
             searchVacanciesUseCase = get()
+        )
+    }
+
+    // ViewModel для экрана настроек фильтров
+    viewModel {
+        FiltersSettingsViewModel(
+            saveFilterSettings = get(),
+            getFilterSettings = get(),
+            clearFilterSettings = get()
+        )
+    }
+
+    // ViewModel для экрана выбора отрасли
+    viewModel {
+        IndustryViewModel(
+            getIndustries = get()
         )
     }
 
